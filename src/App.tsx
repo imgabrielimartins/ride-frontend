fb47c0f037e17911e98fb246f551e93825655063
Pedro <operdop@proton.me>
2026-01-31 15:05:58 -0300

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/footer";
 import Navbar from "./components/navbar/navbar";
 import Home from "./pages/home/Home";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <BrowserRouter>
+        <ToastContainer />
         <Navbar></Navbar>
         <main>
           <Routes>
